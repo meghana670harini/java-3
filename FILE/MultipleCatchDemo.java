ef9c819d7ad4a326c48a9776c6ccc6ca015a77cf
25MCAR103 <meghanap670@gmail.com>
2026-02-12 09:56:48 +0530

    Add MultipleCatchDemo to demonstrate exception handling

---
+++ FILE/MultipleCatchDemo.java
@@ -0,0 +1,18 @@
+MultipleCatchDemo
+{ 
+public static 
+void main(String[] args) { try { int[] 
+arr = {10, 20, 30}; 
+int a = 10 / 0; // ArithmeticException int x = 
+arr[5]; // ArrayIndexOutOfBoundsExcept 
+System.out.println(x); 
+} catch (ArithmeticException e) { 
+System.out.println("Cannot divide by zero!"); 
+} catch (ArrayIndexOutOfBoundsException e) { 
+System.out.println("Array index is invalid!"); 
+} catch (Exception e) { 
+System.out.println("General exception occurred: " + 
+e.getMessage()); 
+} 
+} 
+}
