8335d1dfe991545a708278ab29f315c96c9eb178
25MCAR103 <meghanap670@gmail.com>
2026-02-12 09:59:08 +0530

    Add CombinedDemo.java with error handling example

---
+++ FILE/CombinedDemo.java
@@ -0,0 +1,22 @@
+import java.io.*; 
+public class CombinedDemo 
+{
+static void riskyMethod() throws IOException 
+{ 
+throw new IOException("Simulated I/O error."); 
+} 
+public static void main(String[] args)
+{ 
+try
+{ 
+riskyMethod(); 
+// Must be in try because riskyMethod declar } 
+catch (IOException e) 
+{ 
+System.out.println("Caught in main: " + e.getMessage()); 
+} finally 
+{ 
+System.out.println("Cleanup actions executed here."); 
+} 
+} 
+}
